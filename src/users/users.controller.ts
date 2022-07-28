38306dbc7aa849e8b3694ef96e9314e09244e3b4
Maja <maja.nikolovska@ved.mk>
2022-07-28 13:12:24 +0200

    sign in; sign out

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,26 +17,28 @@ export class UsersController {
 
     }
 
-    @Get('/colors/:color')
-    setColor(@Param('color') color: string, @Session() session: any){
-        //set session value
-        session.color = color;
-    }
-
-    @Get('/colors')
-    getColor(@Session() session: any){
-        //get session value
-        return session.color;
+    @Get('/whoami')
+    whoAmI(@Session() session: any) {
+        return this.userService.findOne(session.userId);
     }
 
     @Post('/signup')
-    createNewUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createNewUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
-    @Post('/sigin')
-    signIn(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    @Post('/signin')
+    async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
     }
 
     // @UseInterceptors(new SerializeInterceptor(UserDto))
