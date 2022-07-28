6642d0ff95f10c434b65e739b1391a5e8d0d00f9
Maja <maja.nikolovska@ved.mk>
2022-07-28 14:13:25 +0200

    current user decorator and interceptor; auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,16 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
+import {
+    Body, Controller, Delete, Get,
+    NotFoundException, Param, Patch,
+    Post, Query, Session, UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -13,13 +20,12 @@ export class UsersController {
     constructor(
         private userService: UsersService,
         private authService: AuthService
-    ) {
-
-    }
+    ) { }
 
     @Get('/whoami')
-    whoAmI(@Session() session: any) {
-        return this.userService.findOne(session.userId);
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user: User) {
+        return user;
     }
 
     @Post('/signup')
