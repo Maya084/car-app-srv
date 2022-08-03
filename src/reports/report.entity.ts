8c969c033eefc535763689022b26608913545790
Maja <maja.nikolovska@ved.mk>
2022-08-03 08:30:09 +0200

    database relations

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,10 +1,32 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "../users/user.entity";
 
 @Entity()
-export class Report{
+export class Report {
     @PrimaryGeneratedColumn()
-    id:number;
+    id: number;
 
     @Column()
     price: number;
+
+    @Column()
+    make: string; //company that made
+
+    @Column()
+    model: string; //model of the vehicle
+
+    @Column()
+    year: number;
+
+    @Column()
+    lng: number;
+
+    @Column()
+    lat: number;
+
+    @Column()
+    mileage: number;
+
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+
+    constructor(private reportsService: ReportsService) {
+
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    @Serialize(ReportDto)
+    createReport(
+        @Body() body: CreateReportDto,
+        @CurrentUser() currentUser: User
+    ) {
+        return this.reportsService.create(body, currentUser)
+    }
+}
