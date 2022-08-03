6edd017809b62519e69f3a388985c79679607eff
Maja <maja.nikolovska@ved.mk>
2022-08-03 11:29:02 +0200

    code cleanup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,9 +25,6 @@ export class UsersController {
     @Get('/whoami')
     @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
-        console.log('aaaaaaaaaaaaaa');
-        console.log(user);
-        
         return user;
     }
 
